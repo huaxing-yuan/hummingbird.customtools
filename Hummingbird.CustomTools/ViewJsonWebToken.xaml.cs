541dc2bfaceba65ca15697f9a4e665e81b9bfbef
Huaxing YUAN <newlucky@gmail.com>
2018-12-19 00:15:24 +0100

    JWT decoding.

--- Hummingbird.CustomTools/ViewJsonWebToken.xaml.cs
+++ Hummingbird.CustomTools/ViewJsonWebToken.xaml.cs
@@ -1,7 +1,10 @@
-﻿using Hummingbird.UI;
+﻿using Hummingbird.TestFramework.Serialization;
+using Hummingbird.UI;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -28,5 +31,34 @@ namespace Hummingbird.CustomTools
             InitializeComponent();
         }
 
+        bool deserialzing = false;
+        bool serializaing = false;
+        private void TxtHeader_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtPayload_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void TxtToken_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!deserialzing)
+            {
+                serializaing = true;
+                JwtSecurityToken jwt = new JwtSecurityToken(txtToken.Text);
+                txtHeader.Text = JsonSerializer.GetInstance(typeof(JsonSerializer)).Serialize(jwt.Header, typeof(JwtHeader));
+                txtPayload.Text = JsonSerializer.GetInstance(typeof(JsonSerializer)).Serialize(jwt.Payload, typeof(JwtPayload));
+                serializaing = false;
+            }
+        }
+
+        private void TxtSecret_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
